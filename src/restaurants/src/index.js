d3487cc7dd29cc47cd0932982b4657dfe7f8a8f6
Ameer Jhan <ameerjhanprof@gmail.com>
2019-12-17 12:13:17 +0530

    feat: add required mf methods in the micro frontends

--- src/restaurants/src/index.js
+++ src/restaurants/src/index.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './pages/app';
 
-ReactDom.render(<App />, document.getElementById('root'));
+const mfName = 'MfRestaurants';
+const mfRenderMethod = `render${mfName}`;
+
+if(process.env.NODE_ENV === 'development') {
+  window['mfRenderMethod'] = mfRenderMethod; 
+}
+
+window[mfRenderMethod] = () => {
+  ReactDom.render(<App />, document.getElementById(mfName));
+};
